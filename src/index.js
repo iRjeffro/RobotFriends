7f303df1678c2d74d1b6ade836dae829a2e92e09
iRjeffro <jg.jeffgordon4@gmail.com>
2023-08-02 21:11:33 -0600

    added searchbox

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
