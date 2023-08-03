9713b5249eefdd7052c3cf335f465933a071ddcc
iRjeffro <jg.jeffgordon4@gmail.com>
2023-08-02 21:44:14 -0600

    added states for searchbox functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriend</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>RoboFriend</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;
