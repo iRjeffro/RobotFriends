7490984f10e5deedb58750b10689700bb9115352
iRjeffro <jg.jeffgordon4@gmail.com>
2023-08-02 23:05:12 -0600

    changed robot list from file to rest api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox'
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        }).then(users => {
+            this.setState({ robots: users });
+        })
+        
     }
 
     onSearchChange = (e) => {
