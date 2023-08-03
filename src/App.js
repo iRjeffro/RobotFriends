79c3ad2e33bed1f99e746020157cf853327b64bf
iRjeffro <jg.jeffgordon4@gmail.com>
2023-08-02 23:08:12 -0600

    ES6 cleanup

--- src/App.js
+++ src/App.js
@@ -13,11 +13,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-            return response.json();
-        }).then(users => {
-            this.setState({ robots: users });
-        })
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({ robots: users }));
         
     }
 
