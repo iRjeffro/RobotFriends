450b23f5854f62a803cce8d29e18b04d43c2ca48
iRjeffro <jg.jeffgordon4@gmail.com>
2023-08-04 19:52:18 -0600

    moved around folder structures

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from './CardList';
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
     constructor() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import App from './App';
+import App from './containers/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
