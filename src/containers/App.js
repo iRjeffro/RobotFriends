bd07ba3340fe097e50c5a12e6326d6e909c2dfb8
iRjeffro <jg.jeffgordon4@gmail.com>
2023-08-31 20:54:26 -0600

    cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll';
